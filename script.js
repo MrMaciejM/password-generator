91a03c1ebd33977c52e3dc9ec13dd161c07c21a2
maciej <63735702+MrMaciejM@users.noreply.github.com>
2022-11-22 19:58:38 +0000

    added if statement to check whether the input matches the requirement of 10 - 64 character length

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // Array of special characters to be included in password
+
 // prettier-ignore
 var specialCharacters = [  "@",  "%", "+", "\\", "/", "'", "!", "#", "$", 
 "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", ".",
@@ -18,8 +19,42 @@ var upperCasedCharacters = [  "A",  "B",  "C",  "D",  "E",  "F",  "G",  "H",  "I
   "S",  "T",  "U",  "V",  "W",  "X",  "Y",  "Z",
 ];
 
+// how many characters / length (at least 10, no more than 64 - you can use .length to count length of strings)
+// confirm special characters
+// include numeric characters
+// lower case chars
+// upper case chars
+
+//var numberArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 // Function to prompt user for password options
-function getPasswordOptions() {}
+function getPasswordOptions() {
+  var pwdLength = 11;
+  var specialChar = true;
+  var numericChar = true;
+  var lowercaseChar = true;
+  var uppercaseChar = true;
+  //prompt("Enter desired password length: min: 10, max: 64");
+
+  while (true) {
+    // first check is to ensure that the user input is a number.
+    // I do this by checking for anything equal to or larger than 0. Any other value will not work, thus ensuring correct value is passed.
+
+    // prettier-ignore
+    if (pwdLength >= 10 && pwdLength <= 64) {
+      console.log("inside number and is a number");
+
+      break;
+    } 
+    
+    // prettier-ignore
+    else {
+      console.log("ensure value is a number between 10 and 64");
+    }
+
+    break;
+  }
+}
+getPasswordOptions();
 
 // Function for getting a random element from an array
 function getRandom(arr) {}
