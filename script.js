20953e365f4ae89430788780e18a39c8bbd0a0a4
maciej <63735702+MrMaciejM@users.noreply.github.com>
2022-11-29 00:23:24 +0000

    fixed issues with confirm not passing through the stored values, fixed issues with double looping

--- script.js
+++ script.js
@@ -21,36 +21,33 @@ var upperCasedCharacters = [  "A",  "B",  "C",  "D",  "E",  "F",  "G",  "H",  "I
 
 // how many characters / length (at least 10, no more than 64 - you can use .length to count length of strings)
 //var pwdLength = 11;
-/*
-var specialChar = false;
-var numericChar = false;
-var lowercaseChar = true;
-var uppercaseChar = true;
-*/
+
 // Function to prompt user for password options
 function getPasswordOptions() {
   //prompt("Enter desired password length: min: 10, max: 64");
-  var pwdLength = 11;
-  var specialChar = true;
-  var numericChar = true;
-  var lowercaseChar = true;
-  var uppercaseChar = false;
-  //prompt("Please enter the desired length of the password: ");
-  while (true) {
-    // first check is to ensure that the user input is a number.
-    if (pwdLength >= 10 && pwdLength <= 64) {
-      //console.log("inside number and is a number");
-      return [
-        pwdLength,
-        specialChar,
-        numericChar,
-        lowercaseChar,
-        uppercaseChar,
-      ];
-    } else {
-      console.log("ensure value is a number between 10 and 64");
-    }
-    break;
+  var pwdLength = prompt("Please enter the desired password length");
+
+  var specialChar = confirm(
+    "Press OK to include special characters in the password, i.e. #, $, !, @"
+  );
+
+  var numericChar = confirm("Press OK to include numbers in your password");
+
+  var lowercaseChar = confirm("Press OK to include lowercase letters");
+
+  var uppercaseChar = confirm("Press OK to include uppercase letters");
+  // first check is to ensure that the user input is a number.
+  if (pwdLength >= 10 && pwdLength <= 64) {
+    console.log("inside IF of getPasswordOptions");
+    // prettier-ignore
+    console.log([pwdLength, specialChar, numericChar, lowercaseChar, uppercaseChar])
+
+    // prettier-ignore
+    var pwdOptions = [pwdLength, specialChar, numericChar, lowercaseChar, uppercaseChar];
+
+    return pwdOptions;
+  } else {
+    console.log("ensure the value is a number between 10 and 64");
   }
 }
 //getPasswordOptions();
@@ -58,21 +55,26 @@ function getPasswordOptions() {
 // Function for getting a random element from an array
 // function getRandom(arr)
 function getRandom() {
-  console.log(getPasswordOptions());
-  console.log(getPasswordOptions()[0]);
+  var pwdOptions = getPasswordOptions();
+  //console.log(getPasswordOptions());
+  //console.log(getPasswordOptions()[0]);
   charArray = [];
-  if (getPasswordOptions()[1] === true) {
-    //console.log("specialChar is true: ");
+  console.log("line 62: " + pwdOptions);
+  if (pwdOptions[1] === true) {
+    console.log("specialChar (pwdOptions) is working: ");
     charArray.push(specialCharacters);
   }
-  if (getPasswordOptions()[2] === true) {
-    //console.log("numericChar is true");
+  console.log("numericChar is: " + pwdOptions[2]);
+  if (pwdOptions[2] === true) {
+    console.log("numericChar is: " + pwdOptions[2]);
     charArray.push(numericCharacters);
   }
-  if (getPasswordOptions()[3] === true) {
+  if (pwdOptions[3] === true) {
+    console.log("lowercaseChar is: " + pwdOptions[3]);
     charArray.push(lowerCasedCharacters);
   }
-  if (getPasswordOptions()[4] === true) {
+  if (pwdOptions[4] === true) {
+    console.log("uppercaseChar is: " + pwdOptions[4]);
     charArray.push(upperCasedCharacters);
   }
   console.log("After if statements");
@@ -80,11 +82,10 @@ function getRandom() {
   var result = "";
   console.log("Chararray before loop is: " + charArray);
 
-  for (var j = 0; j < getPasswordOptions()[0]; j++) {
+  console.log("is it 11??? " + pwdOptions[0]);
+  for (var j = 0; j < pwdOptions[0]; j++) {
     console.log("Char array is: " + charArray);
     var randomArray = Math.floor(Math.random(charArray) * charArray.length);
-    console.log("Line 82: " + charArray);
-    console.log("Line 83: " + randomArray);
     for (var i = 0; i <= charArray[randomArray].length; i++) {
       // random character will be generated with randomChar variable, the array from which this is generated will be random and depends on randomArray variable
       var randomChar = Math.floor(
@@ -96,42 +97,16 @@ function getRandom() {
     result += arrayResult;
   }
   console.log("Result is: " + result);
+  return result;
 
-  switch (randomArray) {
-    case 0:
-      console.log("Entered case 0 - special: " + result);
-      return result;
-    //break;
-    case 1:
-      console.log("Entered case 1 - numeric: " + result);
-      return result;
-    //break;
-    case 2:
-      console.log("Entered case 2 - lower: " + result);
-      return result;
-    //break;
-    case 3:
-      console.log("Entered case 3 - uppercase: " + result);
-      return result;
-    //break;
-  }
+  // removed unnecessary switch statement
 }
 
 // Function to generate password with user input
 function generatePassword() {
-  /*
-var specialChar = true;
-var numericChar = true;
-var lowercaseChar = true;
-var uppercaseChar = true;
-*/
-
-  getPasswordOptions();
-  return getRandom();
-
-  // get password
+  return [getRandom()];
 }
-console.log("pwd generated is: " + generatePassword());
+//console.log("pwd generated is: " + generatePassword());
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -139,6 +114,7 @@ var generateBtn = document.querySelector("#generate");
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+  //var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
